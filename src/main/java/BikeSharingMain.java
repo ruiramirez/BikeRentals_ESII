d0889cb094318e1ebb1a124daa3343810ef6feb9
ruiramirez <ruidanielpc@hotmail.com>
2018-11-12 14:27:21 +0000

    Push da Classe Main

---
+++ src/main/java/BikeSharingMain.java
@@ -0,0 +1,20 @@
+import Exceptions.UserAlreadyExists;
+
+public class BikeSharingMain {
+    public static void main(String[] args) {
+        BikeRentalSystem bRental = new BikeRentalSystem(25);
+
+    try {
+        bRental.registerUser(1,"Rui",1);
+    } catch (UserAlreadyExists userAlreadyExists) {
+        userAlreadyExists.printStackTrace();
+    }
+        bRental.addBicycle(1, 1,1);
+        bRental.addCredit(1, 50);
+        System.out.println(bRental.getDeposits());
+
+        System.out.println(bRental.getUsers());
+
+
+    }
+}
